183d16a96f6553e0f98aa98d06ff8d4822dab489
Brice Earl Johnson <brice@huskers.unl.edu>
2016-02-01 14:50:27 -0600

    Started HW2

---
+++ HW_2/GeneticAlgorithm/src/GeneticAlgorithm.java
@@ -0,0 +1,10 @@
+/**
+ * Created by bricejohnson on 2/1/16.
+ */
+public class GeneticAlgorithm
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
+    }
+}
