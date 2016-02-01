6138b16aab75ebc6f9692f8b99bd4718902a6235
Brice Earl Johnson <bricejohnson@icloud.com>
2016-02-01 15:11:46 -0600

    HW2: Added convertASCIItoString method

--- HW_2/GeneticAlgorithm/src/GeneticAlgorithm.java
+++ HW_2/GeneticAlgorithm/src/GeneticAlgorithm.java
@@ -7,5 +7,24 @@ public class GeneticAlgorithm
     {
         System.out.println("Hello, World");
         System.out.println("Brice Johnson");
+
+        int[] data;
+        data = new int[] {66,114,105,99,101};
+
+        String myName = convertASCIItoString(data);
+    }
+
+    public static String convertASCIItoString(int[] asciiArray)
+    {
+        String word = "";
+
+        for(int i = 0; i < asciiArray.length; i++)
+        {
+            word = word + (char)asciiArray[i];
+        }
+
+        System.out.println(word);
+
+        return word;
     }
 }
